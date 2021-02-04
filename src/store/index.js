0d91daa543709bab0efe9fa5f8002050009a6d98
ichigo-dev <ichigo@web-freework.com>
2021-02-04 12:03:13 +0900

    update

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,23 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    count: 5
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    },
+    decrement(state) {
+      state.count--
+    }
+  },
+  actions: {
+    increment: ({ commit }) => {
+      setTimeout(() => {
+        commit('increment')
+      }, 5000)
+    }
+  },
   modules: {}
 });
